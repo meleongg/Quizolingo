0b8f59f3a65c3f2d0f6a8950ce52eb4d90b380b5
Melvin Teo <62491197+meleongg@users.noreply.github.com>
2022-01-30 12:08:17 -0800

    finished QuizolingoApp implementation, moved main method into Main class

--- src/main/ui/QuizolingoApp.java
+++ src/main/ui/QuizolingoApp.java
@@ -5,42 +5,6 @@ import model.Folder;
 
 import java.util.*;
 
-/*
-add functionality
-- user enters "add", directed to new method
-new method:
-    - You have chosen add
-    - scanner asks for phrase
-    - scanner asks for translation
-    - scanner asks for rating
-    - scanner creates new flashcard and adds it to folder
-    - Flashcard added!
-    - returns to asking for command
-
-remove functionality
-- user enters "remove", directed to new method
-- if there are no flashcards, output there are no flashcards
-    - return to command list
-- if there are flashcards, list the phrases of each
-    - scanner asks for phrase
-    - delete flashcard with corresponding phrase
-    - flashcard removed!
-    - returns to asking for command
-
-update functionality
-- user enters "update proficiency rating"
-- if there are no flashcards, output there are no flashcards
-    - return to command list
-- if there are flashcards,
-    - for each flashcard, list [phrase, rating]
-    - scanner asks for phrase
-    - scanner asks for new rating
-    - update flashcard rating
-    - flashcard updated!
-    - returns to asking for command
-
-*/
-
 // Quizolingo language flashcards application
 public class QuizolingoApp {
     private Scanner input;
@@ -89,6 +53,8 @@ public class QuizolingoApp {
             doAddFlashcard();
         } else if (command.equals("r")) {
             doRemoveFlashcard();
+        } else if (command.equals("v")) {
+            doViewFlashcards();
         } else if (command.equals("u")) {
             doUpdateFlashcard();
         } else {
@@ -104,50 +70,130 @@ public class QuizolingoApp {
         int proficiencyRating;
 
         System.out.println("You chose: Add Flashcard!");
+        input.nextLine(); // this somehow fixes the weird input bug
         phrase = getStringInput("Please enter the flashcard phrase: ");
         System.out.println("You entered phrase as: " + phrase);
         translation = getStringInput("Please enter the flashcard translation: ");
         System.out.println("You entered translation as: " + translation);
-        proficiencyRating = getIntegerInput("Please enter the phrase's proficiency rating: ");
+        proficiencyRating = getIntegerInput("Please enter the phrase's proficiency rating between 1 to 5: ");
         System.out.println("You entered proficiency rating as: " + proficiencyRating);
 
         Flashcard flashcard = new Flashcard(phrase, translation, proficiencyRating);
         this.folder.addFlashcard(flashcard);
     }
 
+    // EFFECTS: displays the names of all the flashcards in folder
+    private void doViewFlashcards() {
+        if (this.folder.getFlashcards().size() == 0) {
+            System.out.println("There are currently no flashcards!");
+        } else {
+            System.out.println("Flashcards currently in this folder: ");
+            for (Flashcard flashcard : this.folder.getFlashcards()) {
+                System.out.println("Flashcard");
+                System.out.println("\t Phrase: " + flashcard.getPhrase());
+                System.out.println("\t Translation: " + flashcard.getTranslation());
+                System.out.println("\t Proficiency Rating: " + flashcard.getProficiencyRating());
+            }
+        }
+    }
+
     // MODIFIES: this
     // EFFECTS: prompts user for input and removes the corresponding flashcard
     //          if the number of flashcards in the folder is non-zero
     private void doRemoveFlashcard() {
+        String phrase;
+        Flashcard flashcard;
+
+        if (this.folder.getFlashcards().size() == 0) {
+            System.out.println("There are no flashcards in this folder!");
+        } else {
+            System.out.println("You chose: Remove Flashcard!");
+            // TODO: list the names of all the flashcards?
+            input.nextLine();
+            phrase = getStringInput("Please enter a flashcard phrase: ");
+            flashcard = this.folder.getFlashcardByPhrase(phrase);
+            this.folder.removeFlashcard(flashcard);
+            // TODO: should I check for phrase DNE? using the getFlashcardByName and checking for dummy return value?
+            System.out.println("Flashcard removed!");
+        }
     }
 
     // MODIFIES: this
     // EFFECTS: prompts user for input and updates the proficiency rating for the
     //          corresponding flashcard if the number of flashcards in the folder is non-zero
     private void doUpdateFlashcard() {
+        String phrase;
+        int proficiencyRating;
+
+        if (this.folder.getFlashcards().size() == 0) {
+            System.out.println("There are no flashcards in this folder!");
+        } else {
+            System.out.println("You chose: Update Flashcard's Proficiency Rating!");
+            // TODO: list the names of all the flashcards?
+            input.nextLine();
+            phrase = getStringInput("Please enter the phrase of the flashcard to update: ");
+            // TODO: should I check for phrase DNE? using the getFlashcardByName and checking for dummy return value?
+            Flashcard flashcard = this.folder.getFlashcardByPhrase(phrase);
+            proficiencyRating = getIntegerInput("Please enter the new proficiency rating: ");
+            flashcard.setProficiencyRating(proficiencyRating);
+            System.out.println("Flashcard updated!");
+        }
     }
 
     // EFFECTS: obtains the user's string input
     private String getStringInput(String message) {
-        System.out.println(message);
-        String userInput = input.nextLine();
+        String userInput = "";
+        boolean checkInputIsString = true;
+
+        while (checkInputIsString) {
+            System.out.println(message);
+            userInput = input.nextLine();
+
+            if ((userInput instanceof String) && checkNonZeroStringLength(userInput)) {
+                checkInputIsString = false;
+            } else {
+                System.out.println("Invalid input!");
+            }
+        }
+
         return userInput;
     }
 
     // EFFECTS: obtains the user's integer input
     private Integer getIntegerInput(String message) {
-        System.out.println(message);
-        int userInput = input.nextInt();
+        int userInput = 0;
+        boolean checkInputIsInt = true;
+
+        while (checkInputIsInt) {
+            System.out.println(message);
+            userInput = input.nextInt();
+            input.nextLine();
+
+            if ((userInput == (int)userInput) && checkIntegerBetweenOneAndFive(userInput)) {
+                checkInputIsInt = false;
+            } else {
+                System.out.println("Invalid Input!");
+            }
+        }
+
         return userInput;
     }
 
     // EFFECTS: produces true if string has a non-zero length, else false
-    private boolean checkNonZeroStringLength() {
+    private boolean checkNonZeroStringLength(String str) {
+        if (str.length() != 0) {
+            return true;
+        }
+
         return false;
     }
 
     // EFFECTS: produces true if integer is [1, 5], else false
-    private boolean checkIntegerBetweenOneAndFive() {
+    private boolean checkIntegerBetweenOneAndFive(int integer) {
+        if ((integer >= 1) && (integer <= 5)) {
+            return true;
+        }
+
         return false;
     }
 
@@ -157,9 +203,5 @@ public class QuizolingoApp {
         for (String command : this.commands) {
             System.out.println("\t" + command.toLowerCase().charAt(0) + " -> " + command);
         }
-    }
-
-    public static void main(String[] args) {
-        new QuizolingoApp();
     }
 }
