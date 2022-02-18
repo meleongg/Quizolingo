17532ebb4177afd8f9e31d70b9d872baa542ee6f
Melvin Teo <62491197+meleongg@users.noreply.github.com>
2022-02-18 00:17:48 -0800

    finished JsonWriterTest tests

--- src/test/persistence/JsonWriterTest.java
+++ src/test/persistence/JsonWriterTest.java
@@ -1,21 +1,23 @@
 package persistence;
 
+import model.Flashcard;
 import model.Folder;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 // class inspired by JsonSerializationDemo (link: https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo)
-public class JsonWriterTest {
+public class JsonWriterTest extends JsonTest {
 
     @Test
     public void testWriterInvalidFile() {
         try {
             Folder folder = new Folder();
-            JsonWriter writer = new JsonWriter("./data/dne:fileName.json");
+            JsonWriter writer = new JsonWriter("./data/\0®fileName.json");
             writer.open();
             fail("IOException was expected");
         } catch (IOException e) {
@@ -23,5 +25,44 @@ public class JsonWriterTest {
         }
     }
 
+    @Test
+    public void testWriterEmptyFolder() {
+        try {
+            Folder folder = new Folder();
+            JsonWriter writer = new JsonWriter("./data/testWriterEmptyFolder.json");
+            writer.open();
+            writer.write(folder);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterEmptyFolder.json");
+            folder = reader.read();
+            assertEquals(0, folder.getFlashcards().size());
+        } catch (IOException e) {
+            fail("Exception should not have been thrown");
+        }
+    }
+
+    @Test
+    public void testWriterGeneralFolder() {
+        try {
+            Folder folder = new Folder();
+            folder.addFlashcard(new Flashcard("bonjour", "hello", 3));
+            folder.addFlashcard(new Flashcard("lardon", "bacon", 2));
+            JsonWriter writer = new JsonWriter("./data/testWriterGeneralFolder.json");
+            writer.open();
+            writer.write(folder);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterGeneralFolder.json");
+            folder = reader.read();
+            assertEquals(2, folder.getFlashcards().size());
+            List<Flashcard> flashcards = folder.getFlashcards();
+            checkFlashcard("bonjour", "hello", 3, flashcards.get(0));
+            checkFlashcard("lardon", "bacon", 2, flashcards.get(1));
+
+        } catch (IOException e) {
+            fail("Exception should not have been thrown");
+        }
+    }
 
 }
